70018efa82dea7e2e7cd5cfba70fad82b176d7e7
michaelstsnow <michael.st.snow@gmail.com>
2018-05-21 23:39:01 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,5 +3,17 @@ def valid_move?
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?
+def position_taken?(board,index)
+  #This method must take in both the existing board and the index that the user
+  #is trying to fill if the index is already occupied it will not allow this
+  position_value=board[index]
+  if position_value != nil
+    if position_value.include? "x" or position_value.include? "o" or position_value.include? "X" or position_value.include? "O"
+      return true
+    else
+      return false
+    end
+  else
+    return false
+  end
 end
